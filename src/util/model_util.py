1d76838e866a5bc742f6072eea5c24793546b2c6
wangqi1996 <1528272574@qq.com>
2020-02-09 17:19:59 +0800

    'embedding'

---
+++ src/util/model_util.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from torch import nn
+
+
+def freeze_params(model: nn.Module) -> None:
+    """
+    Freeze the parameters of this module
+    i.e. do not update them during training
+
+    """
+
+    for _, p in model.named_parameters():
+        p.requires_grad = False
+
+    # for p in model.parameters():
+    #     p.requires_grad = False
